2366df74ba2ec5c27d8e87a5ba88e9d2830d5a0a
Dan Koskei <qimtaid@gmail.com>
2024-03-15 07:12:13 +0300

    Complete lab

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function increment(n) {
+    return n + 1;
+}
+
+function decrement(n) {
+    return n - 1;
+}
+
+// Function to parse integer
+function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  // Function to parse floating point number
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
